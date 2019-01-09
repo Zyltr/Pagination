bd02966321b6698e36455380c0b0c2b58c0f10ba
Zyltr <Zyltr@icloud.com>
2019-01-09 09:22:14 -0800

    Fixed UIPageViewController DataSource function that would return a false Index value

--- Pagination/PageModelController.swift
+++ Pagination/PageModelController.swift
@@ -13,12 +13,11 @@ class PageModelController<Page : Pageable> : NSObject, UIPageViewControllerDataS
     
     private let identifier: String
     private let pageLimit: Int
-    private var presentationIndex: Int
     
     /* Is cleared when all pages loaded */
     private var values: [Page.Value]
     
-    private var viewControllers: [Int : Page]
+    private var pages: [Int : Page]
     private let storyboard: UIStoryboard
     
     
@@ -31,8 +30,7 @@ class PageModelController<Page : Pageable> : NSObject, UIPageViewControllerDataS
         self.values = values
         self.pageLimit = values.count
         self.identifier = viewControllerIdentifier
-        self.presentationIndex = 0
-        self.viewControllers = [:]
+        self.pages = [:]
     }
     
     var firstViewController: Page? {
@@ -44,8 +42,8 @@ class PageModelController<Page : Pageable> : NSObject, UIPageViewControllerDataS
     }
     
     private func viewControllerAtIndex(_ index: Int) -> Page? {
-        if self.viewControllers.keys.contains(index) {
-            return self.viewControllers[index]
+        if self.pages.keys.contains(index) {
+            return self.pages[index]
         }
         
         guard var viewController = self.storyboard.instantiateViewController(withIdentifier: self.identifier) as? Page else {
@@ -59,9 +57,9 @@ class PageModelController<Page : Pageable> : NSObject, UIPageViewControllerDataS
         viewController.pageValue = self.values[index]
         viewController.view.tag = index
         
-        self.viewControllers[index] = viewController
+        self.pages[index] = viewController
         
-        if self.viewControllers.count == self.pageLimit {
+        if self.pages.count == self.pageLimit {
             self.values.removeAll()
         }
         
@@ -75,18 +73,14 @@ class PageModelController<Page : Pageable> : NSObject, UIPageViewControllerDataS
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         let currentIndex = self.indexOfViewController(viewController as! Page)
         let beforeIndex = currentIndex - 1 < 0 ? self.pageLimit - 1 : currentIndex - 1
-        
-        self.presentationIndex = beforeIndex
-        
+
         return currentIndex != beforeIndex ? self.viewControllerAtIndex(beforeIndex) : nil
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         let currentIndex = self.indexOfViewController(viewController as! Page)
         let afterIndex = currentIndex + 1 < self.pageLimit ? currentIndex + 1 : 0
-        
-        self.presentationIndex = afterIndex
-        
+
         return currentIndex != afterIndex ? self.viewControllerAtIndex(afterIndex) : nil
     }
     
@@ -95,7 +89,11 @@ class PageModelController<Page : Pageable> : NSObject, UIPageViewControllerDataS
     }
     
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
-        return self.presentationIndex
+        guard let firstViewController = pageViewController.viewControllers?.first as? Page else {
+            return 0
+        }
+        
+        return self.indexOfViewController(firstViewController)
     }
     
 }
